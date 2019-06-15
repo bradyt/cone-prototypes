8631618537a698c0f341c027bb614ae823b5f80a
Brady Trainor <mail@bradyt.com>
2019-06-14 20:08:57 -0700

    Stuff

--- android/app/src/main/java/com/example/cone_prototypes/MainActivity.java
+++ android/app/src/main/java/com/example/cone_prototypes/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.cone_prototypes;
 
 import android.os.Bundle;
 import io.flutter.app.FlutterActivity;
+import io.flutter.plugin.common.MethodCall;
+import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
+import io.flutter.plugin.common.MethodChannel.Result;
+import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 
 public class MainActivity extends FlutterActivity {
@@ -9,5 +13,28 @@ public class MainActivity extends FlutterActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     GeneratedPluginRegistrant.registerWith(this);
+
+    new MethodChannel(getFlutterView(), CHANNEL).setMethodCallHandler(
+                new MethodCallHandler() {
+                    @Override
+                    public void onMethodCall(MethodCall call, Result result) {
+                      if (call.method.equals("getDropboxStuff")) {
+                        String dropboxStuff = getDropboxStuff();
+
+                        if (dropboxStuff != "") {
+                          result.success(dropboxStuff);
+                        } else {
+                          result.error("UNAVAILABLE", "Dropbox Stuff not available.", null);
+                        }
+                      } else {
+                        result.notImplemented();
+                      }
+                    }
+                });
+
+
+    private String getDropboxStuff() {
+      return "hello world";
+    }
   }
 }
