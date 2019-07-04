a4a66fe2a62d42e554d487af53fbaa0fb41c578f
Brady Trainor <mail@bradyt.com>
2019-07-03 17:41:53 -0700

    Add timestamp, logging, exception handling

--- android/app/src/main/java/info/tangential/cone_prototypes/MainActivity.java
+++ android/app/src/main/java/info/tangential/cone_prototypes/MainActivity.java
@@ -8,6 +8,11 @@ import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugins.GeneratedPluginRegistrant;
+import java.lang.Exception;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
 
 public class MainActivity extends FlutterActivity {
   private static final String CHANNEL = "cone_prototypes.tangential.info/pick_uri";
@@ -21,8 +26,22 @@ public class MainActivity extends FlutterActivity {
     new MethodChannel(getFlutterView(), CHANNEL).setMethodCallHandler(new MethodCallHandler() {
       @Override
       public void onMethodCall(MethodCall call, Result result) {
-        Log.i("pickUri", "Made it!");
+        if (call.method.equals("pickUri")) {
+          String now = now();
+          Log.i("pickUri", now);
+          result.success(now);
+        } else {
+          Log.i("pickUri", "Method " + call.method + " did not equal anything");
+          result.notImplemented();
+        }
       }
     });
+  }
+
+  String now() {
+    TimeZone tz = TimeZone.getTimeZone("UTC");
+    DateFormat df = new SimpleDateFormat("HH:mm:ss.SSS'Z'");
+    df.setTimeZone(tz);
+    return df.format(new Date());
   }
 }
--- lib/channel.dart
+++ lib/channel.dart
@@ -5,7 +5,15 @@ class UriPicker {
       const MethodChannel('cone_prototypes.tangential.info/pick_uri');
 
   static Future<String> pickUri() async {
-    final String uri = await _channel.invokeMethod<String>('pickUri');
+    const name = 'pickUri';
+    String uri;
+    try {
+      uri = await _channel.invokeMethod<String>(name);
+    } on PlatformException catch (e) {
+      throw 'Called $name, caught platform exception $e';
+    } on MissingPluginException {
+      print('$name not implemented');
+    }
     return uri;
   }
 }
